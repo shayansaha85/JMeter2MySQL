06b876976e0023aea876e78ed71d8da55dc1fc6f
Shayan Saha <shayan851997@gmail.com>
2022-08-27 01:10:29 +0530

    Added readme and properties file

--- app.py
+++ app.py
@@ -1,46 +1,94 @@
 from flask import *
 import pymysql.cursors
 import os
+from jproperties import Properties
+
 
 app = Flask(__name__)
+
+
+def getCreds():
+    configs = Properties()
+
+    with open('connection.properties', 'rb') as config_file:
+        configs.load(config_file)
+
+    host = str(configs.get("host").data)
+    user = str(configs.get("user").data)
+    password = str(configs.get("password").data)
+    database = str(configs.get("database").data)
+
+    creds = {
+        "host": "",
+        "user": "",
+        "password": "",
+        "database": ""
+    }
+
+    creds["host"] = host
+    creds["user"] = user
+    creds["password"] = password
+    creds["database"] = database
+
+    return creds
+
 
 def insertResults(jtlFilePath, jmeterBinLocation):
     jtl2html(jtlFilePath, jmeterBinLocation)
     o = getDBData()
     insert_data(o)
 
+
 def getDBData():
     d = get_statistics_data()
-    
+
     output = {
         "totalSamples": d["Total"]["sampleCount"],
-        "minResTime" : round(d["Total"]["minResTime"],2),
-        "avgResTime" : round(d["Total"]["meanResTime"],2),
-        "maxResTime" : round(d["Total"]["maxResTime"],2),
-        "90pct" : round(d["Total"]["pct1ResTime"],2),
-        "95pct" : round(d["Total"]["pct2ResTime"],2),
-        "99pct" : round(d["Total"]["pct3ResTime"],2),
-        "throughput" : round(d["Total"]["throughput"],2)
+        "minResTime": round(d["Total"]["minResTime"], 2),
+        "avgResTime": round(d["Total"]["meanResTime"], 2),
+        "maxResTime": round(d["Total"]["maxResTime"], 2),
+        "90pct": round(d["Total"]["pct1ResTime"], 2),
+        "95pct": round(d["Total"]["pct2ResTime"], 2),
+        "99pct": round(d["Total"]["pct3ResTime"], 2),
+        "throughput": round(d["Total"]["throughput"], 2)
     }
 
     return output
 
+
 def insert_data(d):
-    connection= pymysql.connect(
-        host="localhost",
-        user="root",
-        password="shayan",
-        database="jmeterresults"
+    creds = getCreds()
+    connection = pymysql.connect(
+        host=creds["host"],
+        user=creds["user"],
+        password=creds["password"],
+        database=creds["database"]
     )
 
+    cursor = connection.cursor()
+    sqlForCreatingTable = """
+    create table results (
+	totalSamples int,
+	minResTime decimal(6,2),
+	avgResTime decimal(6,2),
+	maxResTime decimal(6,2),
+	'90pct' decimal(6,2),
+	'95pct' decimal(6,2),
+	'99pct' decimal(6,2),
+	throughput decimal(6,2)
+    )
+    """
+    cursor.execute(sqlForCreatingTable)
 
-    cursor= connection.cursor()
+    print("TABLE CREATED")
 
-    sql="insert into resuts(totalSamples, minResTime, avgResTime, maxResTime, 90pct, 95pct, 99pct, throughput) values (%s, %s, %s, %s,%s, %s, %s, %s)"
+    sql = "insert into resuts(totalSamples, minResTime, avgResTime, maxResTime, 90pct, 95pct, 99pct, throughput) values (%s, %s, %s, %s,%s, %s, %s, %s)"
 
-    cursor.execute(sql, (d["totalSamples"],d["minResTime"],d["avgResTime"],d["maxResTime"],d["90pct"],d["95pct"],d["99pct"],d["throughput"]))
+    cursor.execute(sql, (d["totalSamples"], d["minResTime"], d["avgResTime"],
+                   d["maxResTime"], d["90pct"], d["95pct"], d["99pct"], d["throughput"]))
 
     connection.commit()
+    
     print("DATA INSERTED")
 
 
@@ -65,7 +113,7 @@ def get_statistics_data():
     return d
 
 
-@app.route("/insert", methods = ["POST"])
+@app.route("/insert", methods=["POST"])
 def insert():
     msg = ""
     try:
@@ -73,15 +121,12 @@ def insert():
         jtlFilePath = str(payload["jtl_path"])
         jmeterBinPath = str(payload["jmeter_bin"])
         insertResults(jtlFilePath, jmeterBinPath)
-        msg = {"message" : "results inserted successfully"}
+        msg = {"message": "results inserted successfully"}
     except:
-        msg = {"message" : "error in inserting data"}
+        msg = {"message": "error in inserting data"}
 
     return msg
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
-    
-
-
+    app.run(debug=True, host="0.0.0.0", port=7171)
